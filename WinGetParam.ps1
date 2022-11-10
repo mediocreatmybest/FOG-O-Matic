91ed9840e7cb73a5a6539348cc9e195e3e600f63
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2022-11-11 08:45:50 +0930

    Replaced by another script.
    Replaced by another probably more convoluted script that seems to work. I'm sure it does. ¯\_(ツ)_/¯

--- WinGetParam.ps1
+++
@@ -1,7 +0,0 @@
-#Fog PowerShell wrapper for WinGet snapin
-
-#Find the directory for Winget due to folder changes, why does this need to be changed?!
-param ($wingetcmd, $wingetopt1, $wingetopt2, $wingetopt3, $wingetopt4)
-
-$fullwingetloc = Get-Childitem -Path 'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*' -Include WinGet.exe -Recurse | Select -ExpandProperty FullName
-& "$fullwingetloc" $wingetcmd $wingetopt1 $wingetopt2 $wingetopt3 $wingetopt4
