b621ce926b8dc35edd95cf1d11130ac769d1caba
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2022-11-15 09:17:01 +0930

    Moved some old scripts into CSV info
    Removed old manual scripts, moved information to CSV.

--- FOG-Snapins/WinGet-Script-Install-Pycharm-edu.ps1
+++
@@ -1,6 +0,0 @@
-#Fog PowerShell Script Snapin for WinGet
-
-#Find the directory for Winget due to folder changes, why does this need to be changed?!
-
-$fullwingetloc = Get-Childitem -Path 'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*' -Include WinGet.exe -Recurse | Select -ExpandProperty FullName
-& "$fullwingetloc" install JetBrains.PyCharm.Community -s winget --silent
