87561908debfb09357bb44970e191b4612af2a13
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2022-11-22 12:13:51 +0930

    Should set windows to be default efi loader
    Should set windows to be default efi loader. Need to test and confirm this works.

---
+++ FOG-Snapins/PSConfig-Windows-First-EFI-Bootmgr.ps1
@@ -0,0 +1,5 @@
+﻿#Set Windows to be the first in EFI bootmgr
+#
+#Please See - https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/bcd-system-store-settings-for-uefi?view=windows-11#other-settings
+#
+c:\windows\system32\bcdedit.exe /set “{fwbootmgr}” displayorder “{bootmgr}” /addfirst
