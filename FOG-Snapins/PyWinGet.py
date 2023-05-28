6763c07163350c07c956ef0ec584961e5bae0d74
mediocreatmybest <80406625+mediocreatmybest@users.noreply.github.com>
2023-05-28 14:17:37 +0930

    Added forced commands if 'installing'

--- FOG-Snapins/PyWinGet.py
+++ FOG-Snapins/PyWinGet.py
@@ -1,13 +1,14 @@
 import glob
 import subprocess
 import sys
+
 # Should allow winget commands to be passed directly to winget with an output.
 # Can be converted into an exe with Auto Py to Exe to allow FOG to run WinGet commands
 
 def run_command_with_args(command_args):
     try:
         result = subprocess.run(command_args, check=True)
-        print(f"Command was successfull with return code: {result.returncode} \n")
+        print(f"Command was successful with return code: {result.returncode} \n")
     except subprocess.CalledProcessError as e:
         print(f"Command failed with return code: {e.returncode}")
         sys.exit(e.returncode)
@@ -27,7 +28,18 @@ binary_pattern = r'C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*\*
 binary_path = find_binary(binary_pattern)
 
 # Pass all arguments to the subprocess of winget
-command_args = [binary_path] + sys.argv[1:]
+input_args = sys.argv[1:]
+
+# Additional forced arguments
+forced_args = ['--accept-source-agreements', '--accept-package-agreements', '--silent', '--disable-interactivity']
+
+# Check if any of the inputted arguments contain "install"
+if any('install' in arg.lower() for arg in input_args):
+    # Add the forced arguments if "install" is found
+    command_args = [binary_path] + input_args + forced_args
+else:
+    # Otherwise, use only the inputted arguments
+    command_args = [binary_path] + input_args
 
 # run winget command with args
 run_command_with_args(command_args)
